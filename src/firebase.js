16baf37895731130457188b285bb5352e4556942
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-07-14 10:47:20 +0300

    Creating the react project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYZRYBZv3SqI3iLexbRbKHp64ll0rBpfk",
+    authDomain: "netflix-clone-308f4.firebaseapp.com",
+    projectId: "netflix-clone-308f4",
+    storageBucket: "netflix-clone-308f4.appspot.com",
+    messagingSenderId: "418397188605",
+    appId: "1:418397188605:web:7cea754511aab64f256994"
+  };
