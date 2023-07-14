6d238b7e3d15c5564dfaaad0ba24773c4e37e4df
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-07-14 11:34:11 +0300

    Building new component

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,6 +6,42 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
         <Nav/>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
 
     </div>
   )
--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 function Nav() {
+  const [show,handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll",transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar)
+
+    
+  }, [])
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && 'nav__black'}`}>
         <div className='nav__contents'>
             <img className='nav__logo' 
-            src='https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png'  
+            src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'  
            alt='' />
 
             <img className='nav__avatar' 
