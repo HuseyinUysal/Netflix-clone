72f23ca3d868211bff1b76b8f1342f8a737ae7b4
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-07-15 12:09:36 +0300

    implementing react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
-
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
 
 function App() {
   return (
-      <div className="app">
-      <HomeScreen/>
-      </div>
-  )
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
