1c6a88f6b7c2793430a6bf3c0b061ac8f2a4620a
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-07-18 15:02:22 +0300

    Building ProfileScreen component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  useEffect(() =>{
-    const unsubscribe = auth.onAuthStateChanged(userAuth =>{
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        //Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        //Logged out
+        dispatch(logout());
       }
-    })
+    });
     return unsubscribe;
+  }, [dispatch]);
 
-  }, [])
-  
   return (
     <div className="app">
       <Router>
@@ -27,6 +35,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
--- src/Nav.js
+++ src/Nav.js
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './Nav.css'
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import { useHistory } from "react-router-dom";
 
 function Nav() {
-  const [show,handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
+  const history = useHistory();
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
     } else {
-      handleShow(false)
+      handleShow(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll",transitionNavBar);
-    return () => window.removeEventListener("scroll", transitionNavBar)
-
-    
-  }, [])
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
   return (
-    <div className={`nav ${show && 'nav__black'}`}>
-        <div className='nav__contents'>
-            <img className='nav__logo' 
-            src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'  
-           alt='' />
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+        onClick={() => history.push("/")}
+          className="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt=""
+        />
 
-            <img className='nav__avatar' 
-            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
-            alt='' />
-        </div>
+        <img
+          onClick={() => history.push("/profile")}
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Nav;
